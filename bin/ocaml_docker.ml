a123cdcc78260c94b69a095fa509142ae557fe72
Anil Madhavapeddy <anil@recoil.org>
2017-10-25 13:12:59 +0100

    ocp-indent

--- bin/ocaml_docker.ml
+++ bin/ocaml_docker.ml
@@ -4,71 +4,71 @@ module D = Dockerfile_distro
 module C = Dockerfile_cmd
 
 module Gen = struct
-open Dockerfile
-open Dockerfile_opam
-(* Build the OPAM distributions from the OCaml base *)
-let add_comment ?compiler_version tag =
-  comment "OPAM for %s with %s" tag
-  (match compiler_version with
-      | None -> "system OCaml compiler"
-      | Some v -> "local switch of OCaml " ^ v)
+  open Dockerfile
+  open Dockerfile_opam
+  (* Build the OPAM distributions from the OCaml base *)
+  let add_comment ?compiler_version tag =
+    comment "OPAM for %s with %s" tag
+      (match compiler_version with
+       | None -> "system OCaml compiler"
+       | Some v -> "local switch of OCaml " ^ v)
 
-(* Apk based Dockerfile *)
-let apk_opam2 ?(labels=[]) ~distro ~tag () =
-  header distro tag @@
-  label (("distro_style", "apk")::labels) @@
-  L.Apk.install "build-base bzip2 git tar curl ca-certificates" @@
-  install_opam_from_source ~install_wrappers:true ~branch:"master" () @@
-  run "strip /usr/local/bin/opam*" @@
-  from ~tag distro @@
-  copy ~from:"0" ~src:["/usr/local/bin/opam"] ~dst:"/usr/bin/opam" () @@
-  copy ~from:"0" ~src:["/usr/local/bin/opam-installer"] ~dst:"/usr/bin/opam-installer" () @@
-  L.Apk.install "build-base tar ca-certificates git rsync curl sudo" @@ 
-  L.Apk.add_user ~sudo:true "opam" @@
-  L.Git.init ()
+  (* Apk based Dockerfile *)
+  let apk_opam2 ?(labels=[]) ~distro ~tag () =
+    header distro tag @@
+    label (("distro_style", "apk")::labels) @@
+    L.Apk.install "build-base bzip2 git tar curl ca-certificates" @@
+    install_opam_from_source ~install_wrappers:true ~branch:"master" () @@
+    run "strip /usr/local/bin/opam*" @@
+    from ~tag distro @@
+    copy ~from:"0" ~src:["/usr/local/bin/opam"] ~dst:"/usr/bin/opam" () @@
+    copy ~from:"0" ~src:["/usr/local/bin/opam-installer"] ~dst:"/usr/bin/opam-installer" () @@
+    L.Apk.install "build-base tar ca-certificates git rsync curl sudo" @@ 
+    L.Apk.add_user ~sudo:true "opam" @@
+    L.Git.init ()
 
-(* Debian based Dockerfile *)
-let apt_opam2 ?(labels=[]) ~distro ~tag () =
-  header distro tag @@
-  label (("distro_style", "apt")::labels) @@
-  L.Apt.install "build-essential curl git" @@
-  install_opam_from_source ~install_wrappers:true ~branch:"master" () @@
-  run "strip /usr/local/bin/opam*" @@
-  from ~tag distro @@
-  copy ~from:"0" ~src:["/usr/local/bin/opam"] ~dst:"/usr/bin/opam" () @@
-  copy ~from:"0" ~src:["/usr/local/bin/opam-installer"] ~dst:"/usr/bin/opam-installer" () @@
-  L.Apt.install "build-essential curl git rsync sudo unzip" @@
-  L.Git.init ()
+  (* Debian based Dockerfile *)
+  let apt_opam2 ?(labels=[]) ~distro ~tag () =
+    header distro tag @@
+    label (("distro_style", "apt")::labels) @@
+    L.Apt.install "build-essential curl git" @@
+    install_opam_from_source ~install_wrappers:true ~branch:"master" () @@
+    run "strip /usr/local/bin/opam*" @@
+    from ~tag distro @@
+    copy ~from:"0" ~src:["/usr/local/bin/opam"] ~dst:"/usr/bin/opam" () @@
+    copy ~from:"0" ~src:["/usr/local/bin/opam-installer"] ~dst:"/usr/bin/opam-installer" () @@
+    L.Apt.install "build-essential curl git rsync sudo unzip" @@
+    L.Git.init ()
 
-(* Generate archive mirror *)
-let opam2_mirror (hub_id:string) =
-  header hub_id "alpine-3.6" @@
-  run "sudo apk add --update bash m4" @@
-  run "git clone git://github.com/ocaml/opam-repository /home/opam/opam-repository" @@
-  workdir "/home/opam/opam-repository" @@
-  run "opam admin upgrade" @@
-  run "opam admin cache" @@
-  run "opam init -a /home/opam/opam-repository" @@
-  run "opam install -yj4 cohttp-lwt-unix"
+  (* Generate archive mirror *)
+  let opam2_mirror (hub_id:string) =
+    header hub_id "alpine-3.6" @@
+    run "sudo apk add --update bash m4" @@
+    run "git clone git://github.com/ocaml/opam-repository /home/opam/opam-repository" @@
+    workdir "/home/opam/opam-repository" @@
+    run "opam admin upgrade" @@
+    run "opam admin cache" @@
+    run "opam init -a /home/opam/opam-repository" @@
+    run "opam install -yj4 cohttp-lwt-unix"
 
-let gen_opam_for_distro ?labels d =
-  match D.resolve_alias d with
-  | `Alpine v ->
-       let tag = match v with
-         | `V3_3 -> "3.3" | `V3_4 -> "3.4"
-         | `V3_5 -> "3.5" | `V3_6 -> "3.6"
-         | `Latest -> assert false in
-       Some (D.tag_of_distro d, (apk_opam2 ?labels ~distro:"alpine" ~tag ()))
-  | `Debian v ->
-       let tag = match v with
-         | `V7 -> "7"
-         | `V8 -> "8"
-         | `V9 -> "9"
-         | `Testing -> "testing"
-         | `Unstable -> "unstable"
-         | `Stable -> assert false in
-       Some (D.tag_of_distro d, (apt_opam2 ?labels ~distro:"debian" ~tag ()))
-  | `Ubuntu v ->
+  let gen_opam_for_distro ?labels d =
+    match D.resolve_alias d with
+    | `Alpine v ->
+      let tag = match v with
+        | `V3_3 -> "3.3" | `V3_4 -> "3.4"
+        | `V3_5 -> "3.5" | `V3_6 -> "3.6"
+        | `Latest -> assert false in
+      Some (D.tag_of_distro d, (apk_opam2 ?labels ~distro:"alpine" ~tag ()))
+    | `Debian v ->
+      let tag = match v with
+        | `V7 -> "7"
+        | `V8 -> "8"
+        | `V9 -> "9"
+        | `Testing -> "testing"
+        | `Unstable -> "unstable"
+        | `Stable -> assert false in
+      Some (D.tag_of_distro d, (apt_opam2 ?labels ~distro:"debian" ~tag ()))
+    | `Ubuntu v ->
       let tag = match v with
         | `V12_04 -> "precise"
         | `V14_04 -> "trusty"
@@ -78,65 +78,65 @@ let gen_opam_for_distro ?labels d =
         | `V17_10 -> "artful"
         | _ -> assert false in
       Some (D.tag_of_distro d, (apt_opam2 ?labels ~distro:"ubuntu" ~tag ()))
-  | _ -> None
+    | _ -> None
 end
 
 module Phases = struct
 
-open Rresult
-open R.Infix
+  open Rresult
+  open R.Infix
 
-let arch_to_docker =
-  function
-  | `X86_64 -> "amd64"
-  | `Aarch64 -> "arm64"
+  let arch_to_docker =
+    function
+    | `X86_64 -> "amd64"
+    | `Aarch64 -> "arm64"
 
-let phase1 arch hub_id build_dir logs_dir () =
-  ignore(C.Docker.exists ()); (* TODO rresult *)
-  let build_dir = Fpath.v build_dir in
-  let logs_dir = Fpath.v logs_dir in
-  let joblog = Fpath.(logs_dir / "joblog.txt") in
-  Bos.OS.Dir.create ~path:true build_dir >>= fun _ ->
-  Bos.OS.Dir.create ~path:true logs_dir >>= fun _ ->
-  let d =
-    List.filter (D.distro_supported_on arch) D.active_distros |>
-    List.map Gen.gen_opam_for_distro |>
-    List.fold_left (fun a -> function Some x -> x::a | None -> a) [] in
-  D.generate_dockerfiles ~crunch:false (Fpath.to_string build_dir) d; (* TODO fpath build_dir *)
-  let dockerfile = Fpath.(build_dir / "Dockerfile.{}") in
-  let arch_s = arch_to_docker arch in
-  let gen_tag d = Fmt.strf "%s:linux-%s-opam-%s" hub_id arch_s d in
-  let tag = gen_tag "{}" in
-  let cmd = C.Docker.build_cmd ~cache:false ~dockerfile ~tag (Fpath.v ".") in
-  let args = List.map fst d in
-  C.Parallel.run ~retries:1 ~results:logs_dir ~joblog cmd args >>= fun jobs ->
-  Logs.debug (fun l -> l "joblog: %s" (Sexplib.Sexp.to_string_hum (C.Parallel.sexp_of_t jobs)));
-  List.iter (fun job ->
-    match job.C.Parallel.Joblog.build_logfiles with
-    | None -> ()
-    | Some (stdout,_) ->
-        match C.Docker.build_id (Fpath.v stdout) with
-        | Ok id ->
+  let phase1 arch hub_id build_dir logs_dir () =
+    ignore(C.Docker.exists ()); (* TODO rresult *)
+    let build_dir = Fpath.v build_dir in
+    let logs_dir = Fpath.v logs_dir in
+    let joblog = Fpath.(logs_dir / "joblog.txt") in
+    Bos.OS.Dir.create ~path:true build_dir >>= fun _ ->
+    Bos.OS.Dir.create ~path:true logs_dir >>= fun _ ->
+    let d =
+      List.filter (D.distro_supported_on arch) D.active_distros |>
+      List.map Gen.gen_opam_for_distro |>
+      List.fold_left (fun a -> function Some x -> x::a | None -> a) [] in
+    D.generate_dockerfiles ~crunch:false (Fpath.to_string build_dir) d; (* TODO fpath build_dir *)
+    let dockerfile = Fpath.(build_dir / "Dockerfile.{}") in
+    let arch_s = arch_to_docker arch in
+    let gen_tag d = Fmt.strf "%s:linux-%s-opam-%s" hub_id arch_s d in
+    let tag = gen_tag "{}" in
+    let cmd = C.Docker.build_cmd ~cache:false ~dockerfile ~tag (Fpath.v ".") in
+    let args = List.map fst d in
+    C.Parallel.run ~retries:1 ~results:logs_dir ~joblog cmd args >>= fun jobs ->
+    Logs.debug (fun l -> l "joblog: %s" (Sexplib.Sexp.to_string_hum (C.Parallel.sexp_of_t jobs)));
+    List.iter (fun job ->
+        match job.C.Parallel.Joblog.build_logfiles with
+        | None -> ()
+        | Some (stdout,_) ->
+          match C.Docker.build_id (Fpath.v stdout) with
+          | Ok id ->
             Logs.info (fun l -> l "id %s -> %s" job.C.Parallel.Joblog.arg id);
+          | Error _ -> ()
+      ) jobs;
+    (* TODO check jobs all succeeded *)
+    List.iter (fun job ->
+        match C.Docker.push_cmd (gen_tag job.C.Parallel.Joblog.arg) |> C.run_out with
+        | Ok _ -> ()
         | Error _ -> ()
-  ) jobs;
-  (* TODO check jobs all succeeded *)
-  List.iter (fun job ->
-    match C.Docker.push_cmd (gen_tag job.C.Parallel.Joblog.arg) |> C.run_out with
-    | Ok _ -> ()
-    | Error _ -> ()
-  ) jobs;
-  R.ok ()
+      ) jobs;
+    R.ok ()
 
   let phase2 hub_id () =
     List.iter (fun distro ->
-      let arches = D.distro_arches distro in
-      let platforms = List.map (fun a -> Fmt.strf "linux/%s" (arch_to_docker a)) arches in
-      let template = Fmt.strf "%s:OS-ARCH-opam-%s" hub_id (D.tag_of_distro distro) in
-      let target = Fmt.strf "%s:%s" hub_id (D.tag_of_distro distro) in
-      C.Docker.manifest_push ~platforms ~template ~target |>
-      C.run_out |> R.is_ok |> fun _ -> () (* TODO *)
-    ) D.active_distros;
+        let arches = D.distro_arches distro in
+        let platforms = List.map (fun a -> Fmt.strf "linux/%s" (arch_to_docker a)) arches in
+        let template = Fmt.strf "%s:OS-ARCH-opam-%s" hub_id (D.tag_of_distro distro) in
+        let target = Fmt.strf "%s:%s" hub_id (D.tag_of_distro distro) in
+        C.Docker.manifest_push ~platforms ~template ~target |>
+        C.run_out |> R.is_ok |> fun _ -> () (* TODO *)
+      ) D.active_distros;
     R.ok ()
 
   let phase3 hub_id build_dir () =
@@ -159,7 +159,7 @@ let hub_id =
 let build_dir = 
   let doc = "Directory in which to store build artefacts" in
   Arg.(value & opt file "_build" & info ["b";"build-dir"] ~docv:"BUILD_DIR" ~doc)
- 
+
 let phase1_cmd =
   let logs_dir =
     let doc = "Directory in which to store logs" in
@@ -190,7 +190,7 @@ let phase3_cmd =
   let exits = Term.default_exits in
   Term.(term_result (const Phases.phase3 $ hub_id $ build_dir $ setup_logs)),
   Term.info "phase3-cache" ~doc ~exits
-  
+
 let default_cmd =
   let doc = "build and push opam and OCaml multiarch container images" in
   let sdocs = Manpage.s_common_options in
@@ -199,4 +199,4 @@ let default_cmd =
 
 let cmds = [phase1_cmd; phase2_cmd; phase3_cmd]
 let () = Term.(exit @@ eval_choice default_cmd cmds)
- 
+
