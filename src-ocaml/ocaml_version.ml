35addb87b3729e546f3f1787090bb3d65cd63080
Anil Madhavapeddy <anil@recoil.org>
2017-11-03 20:00:49 +0000

    4.06 out

--- src-ocaml/ocaml_version.ml
+++ src-ocaml/ocaml_version.ml
@@ -98,7 +98,7 @@ module Opam = struct
   let variants {major; minor; _} =
     match major,minor with
     | 4,7 -> ["trunk";"trunk+afl";"trunk+flambda"]
-    | 4,6 -> ["rc1";"rc1+afl";"rc1+flambda";"rc1+default-unsafe-string";"rc1+force-safe-string"]
+    | 4,6 -> ["afl";"flambda";"default-unsafe-string";"force-safe-string"]
     | 4,5 -> ["afl";"flambda"]
     | 4,4 -> ["flambda"]
     | 4,3 -> ["flambda"]
@@ -107,7 +107,7 @@ module Opam = struct
   let default_variant {major; minor; _} =
     match major,minor with
     | 4,7 -> Some "trunk"
-    | 4,6 -> Some "rc1"
+    | 4,6 -> None
     | 4,5 -> None
     | 4,4 -> None
     | 4,3 -> None
