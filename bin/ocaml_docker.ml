da508d26c740dcadb040df5cbbee5cb0951cd3f7
Anil Madhavapeddy <anil@recoil.org>
2017-11-04 15:54:51 +0000

    do not fail cluster rsync

--- bin/ocaml_docker.ml
+++ bin/ocaml_docker.ml
@@ -255,7 +255,7 @@ module Phases = struct
     Bos.OS.File.read_lines Fpath.(build_dir / "pkgs.txt") >>= fun pkgs ->
     let mode = `Remote hosts in
     let cmd = Cmd.(v "./ocaml-docker" % "phase5-build" %% of_list opts % "-vv" % "{}") in
-    C.Mdlog.run_parallel ~mode ~retries:1 md "03-cluster" cmd pkgs >>= fun () ->
+    C.Mdlog.run_parallel ~mode ~retries:1 md "03-cluster" cmd pkgs |> fun _ ->
     let cmd = Cmd.(v "rsync" % "-avz" % "{}:_logs" % ".") in
     C.Mdlog.run_parallel ~mode ~retries:1 md "04-logs" cmd hosts
 end
